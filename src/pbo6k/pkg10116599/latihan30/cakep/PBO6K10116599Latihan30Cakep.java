efe2b17016d7a0739b7e8fb38ef324f7dda9885f
Roby <robyeka@email.unikom.ac.id>
2018-10-01 22:08:58 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan30/cakep/PBO6K10116599Latihan30Cakep.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan30.cakep;
+
+/**
+ *
+ * @author
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : 
+ * 
+ */
+public class PBO6K10116599Latihan30Cakep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Kamu ngerjain sendiri latihan 17 sampe latihan 30 ini?");
+        System.out.print("Yoi/Enggak");
+        
+        
+    }
+    
+}
